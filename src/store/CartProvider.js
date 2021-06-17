06fb153af0f9e9d34c70b78ba3c49cac9cf01aa6
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-06-17 16:56:13 -0400

    Enhanced logic in the reducer dispatch function to avoid addition of duplicate items in the cart, instead update its quantity

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,10 +5,47 @@ const defaultCartState = { items: [], totalAmount: 0 };
 
 const cartReducer = (prevCartState, action) => {
 
-  if (action.type === "ADD_ITEM") {
-    const updatedItems = prevCartState.items.concat(action.item);
+  if (action.type === "ADD_ITEM") 
+  {
+
+    /* Update the total amount based on the item price and item amount */
+
     const updatedTotalAmount =
       prevCartState.totalAmount + action.item.price * action.item.amount;
+
+    /* Find if the item in the cart already exist. This is to ensure we do not add duplicate items but instead just add the quantity of the existing item */
+
+    let existingCartItemIndex = prevCartState.items.findIndex((item)=>item.id === action.item.id)
+    let existingCartItem = prevCartState.items[existingCartItemIndex];
+    let updatedItems;
+
+    /* If the item is found in the cart state, then update its amount and create a new object with the updated amount value */
+
+    if(existingCartItem)
+    {
+        
+        const updatedItem = {
+            ...existingCartItem,
+            amount: existingCartItem.amount+action.item.amount
+        }
+
+        /* Add the cart state items to an updatedItems list. Then update the item with the new updated item object value. */
+
+        updatedItems = [...prevCartState.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+       
+
+    }
+
+
+    else {
+
+        /* If the item is to be added for the first time, then simply add the item to the  cart state*/
+
+        updatedItems = prevCartState.items.concat(action.item);
+
+    }
+    /* In enither case, return the new state of the cart reducer with the updated Item values and total amount */
     return { items: updatedItems, totalAmount: updatedTotalAmount }; //Returns new cart state with updated items and updated total amount
   }
 
